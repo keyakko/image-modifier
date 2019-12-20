9c7e4fdeab8c41a895469308e511441f91383499
keyakko <keyakko.web@gmail.com>
2019-12-20 23:31:21 +0900

    update

--- components/Magick.vue
+++ components/Magick.vue
@@ -1,13 +1,22 @@
 <template>
   <no-ssr>
     <div class="container">
-      <div class="my-12">
-        <input type="file" name="" id="" @change="onImageChanged" />
+      <div class="my-12 bg-gray-300 text-gray-800">
+        <p class="text-center text-lg">image modifier</p>
       </div>
-      <vue-compare-image
-        v-if="image_modified !== ''"
-        :leftImage="image_original.thumbnail"
-        :rightImage="image_modified" />
+      <div class="my-12">
+        <label class="block border-4 border-dashed cursor-pointer border-green-400 bg-green-200 text-green-800 flex items-center justify-center p-8 hover:opacity-50">
+          <p>クリック/ドラックアンドドロップをして画像を選択</p>
+          <input type="file" class="hidden" name="" id="" @change="onImageChanged" />
+        </label>
+      </div>
+      <div class="my-12 border border-gray-800">
+        <vue-compare-image
+          v-if="image_modified !== ''"
+          :leftImage="image_original.thumbnail"
+          :rightImage="image_modified"
+        />
+      </div>
     </div>
   </no-ssr>
 </template>
@@ -55,7 +64,21 @@ export default {
       }
     },
     async doMagick() {
-      const sourceBytes = new Uint8Array(await this.image_original.image.arrayBuffer());
+      Magick.Call([], ['convert', '-list', 'format'])
+      let sourceBytes
+      if (typeof this.image_original.image.arrayBuffer === 'undefined') {
+        // for safari
+        // https://stackoverflow.com/questions/14340989/blob-does-not-accept-uint8array-on-ios
+        const buffer = new ArrayBuffer(this.image_original.image.size);
+        sourceBytes = new Uint8Array(buffer);
+        const f = atob(this.image_original.thumbnail.split(',')[1])
+        for (let i = 0; i < this.image_original.image.size; i++) {
+            sourceBytes[i] = f.charCodeAt(i);
+        }
+      } else {
+        // for chrome
+        sourceBytes = new Uint8Array(await this.image_original.image.arrayBuffer())
+      }
 
       const inputFiles = [{ name: this.image_original.name, content: sourceBytes }]
       let command = []
